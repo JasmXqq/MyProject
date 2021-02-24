3f4c800ced28b55740623393f3319da701e41217
XuQing <1490392335@qq.com>
2021-02-24 09:40:42 +0800

    cbs

---
+++ Src/Excel导出到Cbs/ExcelToCbs/ExcelToCbs/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ExcelToCbs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.buttonEdit_ExportExcel = new DevExpress.XtraEditors.ButtonEdit();
+            this.simpleButton_OutCbs = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.buttonEdit_ExportExcel.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(24, 51);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(94, 22);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "导入Excel：";
+            // 
+            // buttonEdit_ExportExcel
+            // 
+            this.buttonEdit_ExportExcel.Location = new System.Drawing.Point(124, 45);
+            this.buttonEdit_ExportExcel.Name = "buttonEdit_ExportExcel";
+            this.buttonEdit_ExportExcel.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton()});
+            this.buttonEdit_ExportExcel.Size = new System.Drawing.Size(348, 28);
+            this.buttonEdit_ExportExcel.TabIndex = 1;
+            this.buttonEdit_ExportExcel.ButtonClick += new DevExpress.XtraEditors.Controls.ButtonPressedEventHandler(this.buttonEdit_ExportExcel_ButtonClick);
+            // 
+            // simpleButton_OutCbs
+            // 
+            this.simpleButton_OutCbs.Location = new System.Drawing.Point(478, 42);
+            this.simpleButton_OutCbs.Name = "simpleButton_OutCbs";
+            this.simpleButton_OutCbs.Size = new System.Drawing.Size(71, 33);
+            this.simpleButton_OutCbs.TabIndex = 2;
+            this.simpleButton_OutCbs.Text = "导出";
+            this.simpleButton_OutCbs.Click += new System.EventHandler(this.simpleButton_OutCbs_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 126);
+            this.Controls.Add(this.simpleButton_OutCbs);
+            this.Controls.Add(this.buttonEdit_ExportExcel);
+            this.Controls.Add(this.labelControl1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "数据格式转换";
+            ((System.ComponentModel.ISupportInitialize)(this.buttonEdit_ExportExcel.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.ButtonEdit buttonEdit_ExportExcel;
+        private DevExpress.XtraEditors.SimpleButton simpleButton_OutCbs;
+    }
+}
+
---
+++ Src/Excel导出到Cbs/ExcelToCbs/ExcelToCbs/Form1.cs
@@ -0,0 +1,115 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ExcelToCbs
+{
+    public partial class Form1 : Form
+    {
+        private List<Mdl> _mdls = new List<Mdl>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEdit_ExportExcel_ButtonClick(object sender, DevExpress.XtraEditors.Controls.ButtonPressedEventArgs e)
+        {
+            using (var openFile = new OpenFileDialog())
+            {
+                openFile.Multiselect = false;
+                openFile.Filter = @"Excel File(*.xls;*.xlsx)|*.xls;*.xlsx";
+                openFile.Title = @"请选择要导入的数据文件";
+                if (DialogResult.OK == openFile.ShowDialog())
+                {
+                    buttonEdit_ExportExcel.Text = openFile.FileName;
+                    if (Import(openFile.FileName))
+                    {
+                        MessageBox.Show(@"完成数据导入", @"提示!");
+                    }                  
+                }
+            }
+        }
+
+        private bool Import(string path)
+        {
+            _mdls.Clear();
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            {
+                IWorkbook workbook;
+                var extension = Path.GetExtension(path);
+                if (".xlsx".Equals(extension, StringComparison.CurrentCultureIgnoreCase))
+                {
+                    workbook = new XSSFWorkbook(fs);
+                }
+                else
+                {
+                    workbook = new HSSFWorkbook(fs);
+                }
+
+                if (workbook.NumberOfSheets > 0)
+                {
+                    var sheetAt0 = workbook.GetSheetAt(0);
+                    var cou = sheetAt0.LastRowNum;
+                    for (var i = 1; i < cou + 1; i++)
+                    {
+                        var mdl = new Mdl();
+                        var rowHeader = sheetAt0.GetRow(i);
+
+                        mdl.X = rowHeader.GetCell(0).NumericCellValue;
+                        mdl.Y = rowHeader.GetCell(1).NumericCellValue;
+                        mdl.Z = rowHeader.GetCell(2).NumericCellValue;
+                        mdl.Value = rowHeader.GetCell(3).NumericCellValue;
+
+                        _mdls.Add(mdl);
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void simpleButton_OutCbs_Click(object sender, EventArgs e)
+        {
+            using (var saveFile = new SaveFileDialog())
+            {
+                saveFile.Filter = @"Cbs(*.cbs)|*.cbs";
+                saveFile.Title = @"请选择要导出的文件路径";
+                if (DialogResult.OK == saveFile.ShowDialog())
+                {
+                    var strPath = saveFile.FileName;
+                    var sw = new StreamWriter(strPath, false, System.Text.Encoding.Default);
+                    var strText = "";
+                    strText = "点测深属性建模" + "\r\n" + 4 + "\r\n" + _mdls.Count + "\r\n" + "Xlocation" + "\r\n" + "Ylocation" + "\r\n" + "Zlocation" + "\r\n" + "适宜性";
+                    sw.WriteLine(strText);
+                    foreach (var item in _mdls)
+                    {
+                        strText = "";
+
+                        strText = strText + item.X * 100 + "\t";
+                        strText = strText + item.Y * 100 + "\t";
+                        strText = strText + item.Z + "\t";
+                        strText = strText + item.Value;
+                        sw.WriteLine(strText);
+                    }
+                    sw.Close();
+                    MessageBox.Show(@"数据导出完成",@"提示!");
+                }
+            }
+        }
+
+        public class Mdl
+        {
+            public double X { get; set; }
+
+            public double Y { get; set; }
+
+            public double Z { get; set; }
+
+            public double Value { get; set; }
+        }
+    }
+}
